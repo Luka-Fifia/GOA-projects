cac2897b7c47fcd0ccacf90cef524918c3bbfc08
Luka-Fifia <lukapipia58@gmail.com>
2024-09-17 23:19:22 +0400

    fourth commit

---
+++ Phyton course/HW/lvl4/b.py
@@ -0,0 +1,19 @@
+#შექმენით 2 ცვლადი მაგალითად first  და second , input საშვალებით მომხმარებელს შემოაყვანინეთ 2 რიცხვი,
+#შემდეგ ამ ორი ცვლადის საშვალებით გააკეთეთ მათემატიკური ოპერაციები (+   ,   -   ,   /   ,   *   )
+
+
+first = input("enter first number ")
+
+
+second = input("enter second number ")
+
+a = "+"
+b = "-"
+c = "*"
+d = "/"
+
+
+print(a + "   " + int(first) + int(second))
+print(int(first) - int(second))
+print(int(first) * int(second))
+print(int(first) / int(second))
---
+++ Phyton course/HW/lvl4/c.py
@@ -0,0 +1,2 @@
+user1 = input("enter your name: ")
+print(user1)
